6df4dc66958b8e97dc97cc0f24db86cf8fb29c06
Douxcaramel3 <estefesfer@hotmail.com>
2023-06-16 20:10:33 +0200

    160623

--- IndicatuEdad/js/script.js
+++ IndicatuEdad/js/script.js
@@ -1,14 +1,17 @@
 function calEdad(){
-    document.getElementById("nsnEdad").innerHTML="";
-    document.getElementById("msnError").innerHTML="";
+    document.getElementById("msnEdad").innerHTML=" ";
+    document.getElementById("msnError").innerHTML=" ";
 
 let nEdad=document.getElementById("edad").value;
+
+
+
 let msnEdad="";
 let msnError="";
 console.log(nEdad)
 if (nEdad=="" || nEdad<0 || nEdad>99){
-    msnError="indique edad válida"
-    document.getElementById("msnError").innerHTML="msnError";
+    msnError="indique edad válida";
+    document.getElementById("msnError").innerHTML=msnError;
     
 }else if(nEdad<12){
     msnEdad="Aún eres un niño";
@@ -17,7 +20,7 @@ if (nEdad=="" || nEdad<0 || nEdad>99){
 }else if (nEdad<=40){
     msnEdad="Eres un viejoven";
 }else{
-    msnEdad="Ciudate mucho! Ya vas en edad!"
+    msnEdad="Ciudate mucho! Ya vas en edad!";
 }
 document.getElementById("msnEdad").innerHTML=msnEdad;
 }
---
+++ QuintoJS/js/script.js
@@ -0,0 +1,128 @@
+/**
+ * if-else -> También existe otra estructura de control de flujo switch-case
+ * Su función es: según el parametro o valor que contenga, puede realizar una serie de opciones
+ * Ej:
+ * switch(expresión) {
+ * case x:
+ * // code block
+ * break; // para salir del switch y no seguir evaluando los siguientes casos case y:
+ * // code block
+ * break;
+ * default:
+ * // code block
+ * }
+ * 
+ Así es como funciona:
+
+ La expresión de cambio te evalúa un vez
+ El valor de la expresión se compara con los valores de cada caso.
+ Si hay una coincidencia, se ejecuta el bloque de codigo asociado
+ Si no hay ayuda coincidencia, se ejecuta el bloque de codigo
+ * 
+ */
+
+
+ //Ejemplo sencillo
+ let dia=7
+ console.log (dia);
+ let diaS;
+ switch (dia){
+    case 1:
+        diaS="Lunes";
+        break;
+    case 2:
+        diaS="Martes";
+        break;
+    case 3:
+        diaS="Miercoles";
+        break;
+    case 4:
+        diaS="Jueves";
+        break;
+    case 5:
+        diaS="Viernes";
+        break;
+    case 6,7:
+        diaS="Fin de semana";
+        break;
+    
+    default:
+        diaS="Este día no existe";
+ }
+
+document.getElementById("dia").innerHTML=diaS;
+
+/**
+ * Ejercicio
+ * 
+ * De la base de la puntuación numérica de los alumos indicamos que del 0 al 4 -> Insuficiente
+ *       0 al 4 -> Suspenso
+ *       5 al 6 -> Bien
+ *       7 al 8 -> Notable
+ *       9 al 10 ->Sobresaliente
+ *
+ */
+
+function calNota(){
+    
+
+let nota=parseInt(document.getElementById("examen").value);
+
+console.log(nota);
+let msnNotas;
+
+switch (nota){
+    case 0:
+        case 1:
+            case 2:
+                case 3:
+                    case 4:
+        msnNotas="Suspenso";
+        break;
+    case 5:
+        case 6:
+        msnNotas="Bien";
+        break;
+    case 7:
+        case 8:
+        msnNotas="Notable";
+        break;
+    case 9:
+        case 10:
+        msnNotas="Sobresaliente";
+        break;
+    default:
+        msnNotas="Lo siento, no tienes nota";
+
+}
+document.getElementById("nota").innerHTML=msnNotas;
+}
+
+/**
+ * for (let i=1; i<); i++)
+ * 
+ * Ejercicio: Mostrar en consola los numeros del 1 al 20 de mayor a menor
+ * 
+ * for (let i=20; i>0; i--)
+ * 
+ */
+
+for (let i=20; i>0; i=i-2){
+    console.log(i);
+}
+
+// bucle for each--> 
+
+function cambiarColor(){
+let colores=["blue","green","yellow","pink","orange","purple"]
+let indice=parseInt(document.getElementById("numero").value);
+--indice;
+
+for (let i=0; i<colores.length; i++){
+
+ if(i==indice){
+//cambiar el color a el div caja
+document.getElementById("caja").style.backgroundColor=colores[i];
+ }
+}
+}
