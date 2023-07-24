1bd5c8b75b61768e3e33ef380153bc5ca791e2df
Douxcaramel3 <estefesfer@hotmail.com>
2023-07-24 21:39:01 +0200

    cambios casa

--- ZOO/js/script.js
+++ ZOO/js/script.js
@@ -1,4 +1,12 @@
+alert("Bienvenido! suscribete para tener más noticias")
+
+window.addEventListener("load",function(){
+  const d=new Date();
+  this.document.querySelector("#fecha").innerHTML=d.getDate() + "/" + (d.getMonth()+1)+"/"+d.getFullYear(); 
+  this.document.querySelector("#hora").innerHTML=d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  
 function openPage(pageName,elmnt,color) {
+
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -11,5 +19,6 @@ function openPage(pageName,elmnt,color) {
     document.getElementById(pageName).style.display = "block";
     elmnt.style.backgroundColor = color;
   }
-  
- 
+})
+
+
