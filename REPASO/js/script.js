fe45ba675b9348e94cd88cb6ef49be8a73a1edc4
Douxcaramel3 <estefesfer@hotmail.com>
2023-08-04 13:52:17 +0200

    040823

---
+++ REPASO/js/script.js
@@ -0,0 +1,17 @@
+Swal.fire({
+confirmButtonText: 'Sign in',
+  focusConfirm: false,
+  preConfirm: () => {
+    const login = Swal.getPopup().querySelector('#login').value
+    const password = Swal.getPopup().querySelector('#password').value
+    if (!login || !password) {
+      Swal.showValidationMessage(`Please enter login and password`)
+    }
+    return { login: login, password: password }
+  }
+}).then((result) => {
+  Swal.fire(`
+    Login: ${result.value.login}
+    Password: ${result.value.password}
+  `.trim())
+})
